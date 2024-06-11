d525214e16c46cf1453912b543719dfd3008932a
gurodrigues-dev <gustavorodrigueslima2004@gmail.com>
2024-06-11 09:07:31 -0300

    feat: initial commit

---
+++ src/main/java/com/technology/venture/vtx_ses_email_ms/VtxSesEmailMsApplication.java
@@ -0,0 +1,13 @@
+package com.technology.venture.vtx_ses_email_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VtxSesEmailMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VtxSesEmailMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/technology/venture/vtx_ses_email_ms/VtxSesEmailMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.technology.venture.vtx_ses_email_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VtxSesEmailMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
